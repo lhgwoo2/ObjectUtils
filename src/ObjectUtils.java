6925c913fae21525607a81ffc905fff7a6cc2d36
lhgwoo <lhgwoo2@naver.com>
2018-01-26 13:49:25 +0900

    string형태의 empty 판별

--- src/ObjectUtils.java
+++ src/ObjectUtils.java
@@ -9,6 +9,16 @@ public class ObjectUtils {
     if((s instanceof String) && ((String)s).trim().length()==0) {
       return true;
     }
+    /**
+     *  위의 방법으로 null 확인을 하기도 하나, String클래스의 경우 isEmpty()-(길이가 0인지 판별)가 존재함으로
+                  해당방식으로도 많이 사용한다.
+        
+        String s = null or "";
+        if(s == null || s.isEmpty()){
+          return true;
+        }
+     */
+    
     if(s instanceof Map) {
       return ((Map<?, ?>) s).isEmpty();
     }
